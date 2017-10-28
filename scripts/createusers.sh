f28bd0577ba8f0c43f1b59b759261320b4b5b0a9
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-10-28 15:47:30 +0200

    Script aangepast om alle gebruikers automatisch te genereren
    Paswoord is de stad van de gebruiker

--- scripts/createusers.sh
+++ scripts/createusers.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-INPUT=avalon-employees.csv
+INPUT=../doc/avalon-employees.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read Number GivenName Surname Username Title Unit Gender StreetAddress Postcode City Co
-untry CountryFull TelephoneNumber Birthday
+while read Number GivenName Surname Username Title Unit Gender StreetAddress Postcode City Country CountryFull TelephoneNumber Birthday
 do
-        echo "name: $Username"
-        echo "comment: $GivenName $Surname $Title $Unit "
-        echo "password: $(openssl passwd -salt 2 -1 test)"
+        echo "- name: $Username"
+        echo "  comment: $GivenName $Surname $Title"
+        echo "  password: $(openssl passwd -salt 2 -1 $City)"
+        echo "  shell: /sbin/nologin"
+        echo "  groups: $Unit"
 done < $INPUT
 IFS=$OLDIFS
